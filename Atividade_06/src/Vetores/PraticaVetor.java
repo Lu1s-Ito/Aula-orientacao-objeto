ecfceea8970233090fed1951f6988e88e89ba8c5
LUIS HENRIQUE DA SILVA ITO <luis.hsito@senacsp.edu.br>
2026-05-26 11:39:43 -0300

    Atualizacao 26/05

---
+++ Atividade_06/src/Vetores/PraticaVetor.java
@@ -0,0 +1,81 @@
+//Pacote que será trabalhado
+package Vetores;
+
+//adicionar a biblioteca
+import java.util.Arrays;
+
+//classe a ser trabalhada
+public class PraticaVetor {
+
+    //roda na memoria
+    public static void main(String[] args) {
+        
+        //declara o vetor
+        int[] numeros = {5, 25, 45, 50, 65, 75, 80, 85, 90, 95};
+        System.out.println("Numeros originais: " + Arrays.toString(numeros));
+        
+        //soma
+        int soma = 0;
+        
+        //maior
+        int maior = numeros[0];
+        
+        //menor
+        int menor = numeros[0];
+        
+        //usando for-each
+        for (int num : numeros) {
+            soma += num;
+            
+            if(num > maior)
+                maior = num;
+            
+            if(num < menor)
+                menor = num;
+        }
+        
+        //media
+        double media = (double) soma / numeros.length;
+        
+        //imprime a soma, maior e menor
+        System.out.println("Soma: " + soma);
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+        System.out.println("Media: " + media);
+        System.out.println("==========================================================");
+        
+        //inserir mais um vetor
+        int[] novoVetor = new int[numeros.length + 2];
+        for (int i = 0, j = 0; i < novoVetor.length; i++) {
+            
+            if (i == 4){
+                novoVetor[i] = 55; // Insere o 55 na 3 posição
+            }
+            
+            else if (i == 1) {
+                novoVetor[i] = 20;
+            
+            }else {
+                novoVetor[i] = numeros[j]; // Copia o valor do vetor original
+                j++; // Só avança o j se copiamos do vetor original
+            }
+        }
+        
+        //remove o utimo indice
+        int[] removeVetor = new int[novoVetor.length -1];
+        for (int i = 0, j = 0; i < removeVetor.length; i++)
+            
+            if (i != 12) {
+                removeVetor[j] = novoVetor[i];
+                j++;
+            }
+         
+        
+        //imprime os numeros inseridos e removidos
+        System.out.println("Inserir e remover");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Numeros com insert : " + Arrays.toString(novoVetor));
+        System.out.println("Remove o utimo numero: " + Arrays.toString(removeVetor));
+    }
+        
+}
--- Atividade_06/src/Vetores/VetorIR.java
+++ Atividade_06/src/Vetores/VetorIR.java
@@ -1,6 +1,7 @@
 //Pacote que será trabalhado
 package Vetores;
 
+//adicionar a biblioteca
 import java.util.Arrays;
 
 //classe a ser trabalhada
@@ -11,25 +12,39 @@ public class VetorIR {
         
         //declara o vetor
         int[] numeros = {10,20,30,40,50};
+        System.out.println("Numeros originais: " + Arrays.toString(numeros));
 
         //inserindo o novo indice e valor
         int[] novoVetor = new int[numeros.length + 1];
         
-        for (int i = 0, j = 0; i < numeros.length; i++){
+        // Inserindo o novo índice e valor
+        for (int i = 0, j = 0; i < novoVetor.length; i++) {
             
             if (i == 2) {
-                novoVetor[i] = 25;
-            }else{
-            
-                novoVetor[i] = numeros[j];
-                j++;
-                
+                novoVetor[i] = 25; // Insere o 25 na posição 2
+            } else {
+                novoVetor[i] = numeros[j]; // Copia o valor do vetor original
+                j++; // Só avança o j se copiamos do vetor original
             }
-            
         }
         
-        //imprime os novos indices e valores
-        System.out.println("Numeros " + Arrays.toString(novoVetor));
+        //remover o vetor no indice 3
+        int[] vetorEmovido = new int[novoVetor.length -1];
+        
+        //processo que ira remover o vetor
+        // Remover o vetor no índice 3
+            for (int i = 0, j = 0; i < novoVetor.length; i++) {
+
+                if (i != 3) { // Ignora o índice 3 para fazer a "remoção"
+                    vetorEmovido[j] = novoVetor[i];
+                    j++;
+                }
+            }
+
+         //imprime os novos indices e valores
+        System.out.println("Numeros com insert : " + Arrays.toString(novoVetor));
+        System.out.println("Numeros Removido:  " + Arrays.toString(vetorEmovido));
+        
+        }
+        
     }
-    
-}
